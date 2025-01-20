2af231da19fc471690cd74fd49c0872aca4a4811
Arthur Gabriel Lima Gomes <78980894+ArthurGabrieel@users.noreply.github.com>
2025-01-20 17:01:16 -0300

    candy right solution
    Co-Authored-By: Alexandre Beck <82113486+zzzBECK@users.noreply.github.com>

--- 135-candy.ts
+++ 135-candy.ts
@@ -2,9 +2,17 @@ function candy(ratings: number[]): number {
   const n = ratings.length;
   const candies = new Array(n).fill(1);
 
+  // Da esquerda para a direita
   for (let i = 1; i < n; i++) {
     if (ratings[i] > ratings[i - 1]) {
       candies[i] = candies[i - 1] + 1;
+    }
+  }
+
+  // Da direita para a esquerda
+  for (let i = n - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1]) {
+      candies[i] = Math.max(candies[i], candies[i + 1] + 1);
     }
   }
 
